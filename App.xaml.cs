ae5c6727940682f1d3b2030fd276ab599d4be1d5
juupant <amunt2002@gmail.com>
2025-04-14 12:50:42 +0300

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kolmas_view
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace kolmas_view
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
