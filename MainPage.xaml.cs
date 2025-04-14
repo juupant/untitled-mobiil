c91bb8cab61d38f31068e8b868308d0d93bc12ca
juupant <amunt2002@gmail.com>
2025-04-14 13:05:18 +0300

    xaml design start

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,55 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private double hunger = 1.0;
+        private double fun = 1.0;
+        private readonly double decreaseRate = 0.1;
+        private readonly double increaseAmount = 0.2;
+        private IDispatcherTimer timer;
 
         public MainPage()
         {
             InitializeComponent();
+            StartDecreaseTimers();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void StartDecreaseTimers()
         {
-            count++;
+            timer = Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += (s, e) => DecreaseStats();
+            timer.Start();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            timer?.Stop();
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void DecreaseStats()
+        {
+            hunger = Math.Max(0, hunger - decreaseRate);
+            fun = Math.Max(0, fun - decreaseRate);
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            HungerBar.Progress = hunger;
+            FunBar.Progress = fun;
+        }
+
+        private void OnFeedClicked(object sender, EventArgs e)
+        {
+            hunger = Math.Min(1.0, hunger + increaseAmount);
+            UpdateUI();
+        }
+
+        private void OnPlayClicked(object sender, EventArgs e)
+        {
+            fun = Math.Min(1.0, fun + increaseAmount);
+            UpdateUI();
         }
     }
-
 }
